15f6110f1ca0beacdae1c17746b50da7e4a752cb
liaoshuhao <lgoodbook@163.com>
2017-06-16 11:06:48 +0800

    111

---
+++ src/main/java/cn/gb/wechat/model/package-info.java
@@ -0,0 +1,36 @@
+/******************************************************************
+ *
+ *    Java Lib For Android, Powered By Shenzhen Jiuzhou.
+ *
+ *    Copyright (c) 2001-2014 Digital Telemedia Co.,Ltd
+ *    http://www.d-telemedia.com/
+ *
+ *    Package:     cn.gb.wechat.model
+ *
+ *    Filename:    package-info.java
+ *
+ *    Description: TODO(用一句话描述该文件做什么)
+ *
+ *    Copyright:   Copyright (c) 2001-2014
+ *
+ *    Company:     Digital Telemedia Co.,Ltd
+ *
+ *    @author:     DELL
+ *
+ *    @version:    1.0.0
+ *
+ *    Create at:   2017年5月25日 上午11:39:32
+ *
+ *    Revision:
+ *
+ *    2017年5月25日 上午11:39:32
+ *        - first revision
+ *
+ *****************************************************************/
+/**
+ * 
+ * @author liaosh
+ * @Date 2017年5月25日 上午11:39:32
+ * @version 1.0.0
+ */
+package cn.gb.wechat.model;
